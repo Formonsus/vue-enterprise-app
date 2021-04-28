c3009c0fc1624939b99d53e1d54ea7e0a2ad1e7f
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-28 11:59:41 +0200

    refactor(application): refactor Home.vue
    Refactored Home.vue page to use newly created Layout component.

--- application/src/router/views/Home.vue
+++ application/src/router/views/Home.vue
@@ -1,18 +1,19 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
+import Layout from "../layouts/MainLayout.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Layout
   }
 };
 </script>
+
+<template>
+  <Layout class="Home">
+    <img alt="Vue logo" src="../../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </Layout>
+</template>
