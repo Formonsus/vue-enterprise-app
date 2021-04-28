c18a441e2cf94700c6134771a9342f161224a728
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-28 11:47:49 +0200

    refactor(application): main.js refactor
    Created app variable where we are storing our Vue app after createApp function is called.

--- application/src/main.js
+++ application/src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
+
 import App from "./App.vue";
 import router from "./router";
 import state from "./state";
 
-createApp(App).use(state).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(state);
+app.use(router);
+
+app.mount("#app");
