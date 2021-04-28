b777328b3c0c397a79cd4f8b2bda3eefccd704eb
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-28 12:04:05 +0200

    feat(application): add 404 page
    Created _404.vue component that will be used as 404 page.

---
+++ application/src/router/views/_404.vue
@@ -0,0 +1,32 @@
+<script>
+import Layout from "../layouts/MainLayout.vue";
+
+export default {
+  components: { Layout },
+  props: {
+    resource: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<template>
+  <Layout class="notFound">
+    <h1>
+      404
+      <template v-if="resource">
+        {{ resource }}
+      </template>
+    </h1>
+  </Layout>
+</template>
+
+<style lang="scss">
+.notFound {
+  h1 {
+    text-align: center;
+  }
+}
+</style>
