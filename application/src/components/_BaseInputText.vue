3da6019a9bfc329e3854f5a3a80a9a62c3da98d4
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-29 12:05:29 +0200

    feat(application): baseInputText component

---
+++ application/src/components/_BaseInputText.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    type: {
+      type: String,
+      default: "text",
+      validator(value) {
+        return ["email", "password", "search", "tel", "text", "url"].includes(value);
+      }
+    },
+    modelValue: {}
+  }
+};
+</script>
+
+<template>
+  <input :type="type" :value="modelValue" v-bind="$attrs" @input="$emit('update:modelValue', $event.target.value)" />
+</template>
