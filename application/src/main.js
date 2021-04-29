42d54e74413c44d5cfeee0701fbe8c52113f4b26
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-29 12:23:54 +0200

    feat(application): call registerGlobalComponents
    In main.js file called new method registerGlobalComponents

--- application/src/main.js
+++ application/src/main.js
@@ -3,8 +3,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import state from "./state";
+import registerGlobalComponents from "./components/_globals";
 
 const app = createApp(App);
+
+// Globally register all `_Base` prefixed components.
+registerGlobalComponents(app);
 
 app.use(state);
 app.use(router);
