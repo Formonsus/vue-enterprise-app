2dd3764828ab63c28ace8b88abeb843a68012cd6
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-04-29 11:46:48 +0200

    feat(application): 404 route
    Add 404 route and created catch for any unmatched route that will redirect to 404 page

--- application/src/router/routes.js
+++ application/src/router/routes.js
@@ -13,5 +13,16 @@ export default [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
+  },
+  {
+    path: "/404",
+    name: "404",
+    component: () => import("./views/_404.vue"),
+    props: true
+  },
+  // Redirect any unmatched routes to 404 page.
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "404"
   }
 ];
